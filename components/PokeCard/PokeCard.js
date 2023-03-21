7c985488daa5dfeb9a4cb22df4d5d800bd3f2947
John Clarke <themagoo@hotmail.co.uk>
2023-03-21 20:47:56 +0000

    Initial commit

---
+++ components/PokeCard/PokeCard.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+import useSWR from 'swr'
+
+export default function PokeCard({id, ...props}) {
+
+  const fetcher = url => axios.get(url).then(res => res.data)
+
+  const { data: pokemon, error } = useSWR(`https://pokeapi.co/api/v2/pokemon/${id}`, fetcher) 
+
+  return (
+    <div
+      key={id}
+      className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+    >
+
+      <div className="aspect-w-3 aspect-h-4 bg-gray-200 group-hover:opacity-75 sm:aspect-none sm:h-96">
+        <img
+          className="h-full w-full object-cover object-center sm:h-full sm:w-full" 
+          src={`https://raw.githubusercontent.com/sashafirsov/pokeapi-sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+        />
+      </div>
+      
+      <div className="flex flex-1 flex-col space-y-2 p-4">
+        <h3 className="text-md font-large text-gray-900">
+            {pokemon?.name?.toUpperCase()}
+        </h3>
+        <div className="flex flex-1 flex-col justify-end">
+          <p>HP: {pokemon?.stats?.find(({stat}) => stat.name === 'hp')['base_stat']}</p>
+          <p>Attack: {pokemon?.stats?.find(({stat}) => stat.name === 'attack')['base_stat']}</p>
+          <p>Defense: {pokemon?.stats?.find(({stat}) => stat.name === 'defense')['base_stat']}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
